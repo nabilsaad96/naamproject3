c62e92bae3f5a3328c9e99de48b421a6abc3ad11
nabilsaad96 <nabil96@vt.edu>
2018-04-16 18:04:49 -0400

    asdf
    sadf

--- web/app/config.php
+++ web/app/config.php
@@ -2,7 +2,7 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL','http://ec2-18-220-174-91.us-east-2.compute.amazonaws.com/GUI_P5/project5'); # your base URL
+define('BASE_URL','http://ec2-18-220-174-91.us-east-2.compute.amazonaws.com/naamproject3/web'); # your base URL
 
 define('DB_USER', 'root');
 define('DB_PASS', 'AmazonSQL!');
