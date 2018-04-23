07747b5dae50f779a3d1b6c301e9451d38c557a7
nabilsaad96 <nabil96@vt.edu>
2018-04-23 16:10:50 -0400

    sdf
    sdf

--- web/app/config.php
+++ web/app/config.php
@@ -7,6 +7,6 @@ define('BASE_URL','http://ec2-18-220-174-91.us-east-2.compute.amazonaws.com/naam
 define('DB_USER', 'root');
 define('DB_PASS', 'AmazonSQL!');
 define('DB_HOST', 'localhost');
-define('DB_DATABASE', 'articles');
+define('DB_DATABASE', 'naam');
 
 date_default_timezone_set('America/New_York');
