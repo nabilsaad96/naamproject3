c6a5cdf98851fca03079c8a58b076493f5e4e9a4
nabilsaad96 <nabil96@vt.edu>
2018-04-26 17:20:31 -0400

    sfd
    ds

--- web/app/model/Application.class.php
+++ web/app/model/Application.class.php
@@ -86,6 +86,7 @@ class Application {
 
         $r->$keys[0]             = $row[$keys[0]];//'Aname'];
         echo($r->$keys[0]);
+        echo('  ||||  ');
         echo(array_keys($r));
         echo('  ||||  ');
         echo($keys[0]);
