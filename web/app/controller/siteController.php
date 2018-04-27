1e69e79686f52739e541f378497fd3bcfcf30f87
nabilsaad96 <nabil96@vt.edu>
2018-04-27 01:16:37 -0400

    sfds
    df

--- web/app/controller/siteController.php
+++ web/app/controller/siteController.php
@@ -359,7 +359,7 @@ class SiteController {
 		$pageTitle = 'Ad Hoc Query';
 		include_once SYSTEM_PATH.'/view/header.tpl';
 		$rl = Application::doQuery($q);
-		$keys = array_keys[$rl[0]];
+		$keys = array_keys($rl[0]);
 		echo($keys);
 		//$rl = Application::loadAll();
 
