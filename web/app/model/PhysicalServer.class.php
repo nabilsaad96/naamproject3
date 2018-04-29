13f5abf0f6b8ddc600872f939ad7cc89a01427b4
nabilsaad96 <nabil96@vt.edu>
2018-04-29 14:35:29 -0400

    sadf
    asdf

--- web/app/model/PhysicalServer.class.php
+++ web/app/model/PhysicalServer.class.php
@@ -163,7 +163,7 @@ SELECT Xname FROM Virtualizes WHERE Sname='%s';", $id,$id,$id,$id,$id);
   public static function updateAdmin($name, $admin) {
       $db = Db::instance();
       // Database query
-      $q = sprintf("UPDATE '%s' SET admin = '%s' WHERE Sname = '%s';", self::DB_TABLE, $admin, $name);
+      $q = sprintf("UPDATE `%s` SET admin = '%s' WHERE Sname = '%s';", self::DB_TABLE, $admin, $name);
       echo($q);
       // Do the query
       $result = $db->query($q);
